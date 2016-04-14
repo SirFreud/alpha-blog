5175730ccec421ebd68bdba772573d3d303a9e12
SirFreud <mikevaldez92@gmail.com>
2016-04-14 01:34:15 -0500

    Added both the edit and update functions to the articles_controller and also added the edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find_by(params[:id])
+  end
+
   def create
 
     @article = Article.new(article_params)
@@ -19,6 +23,16 @@ class ArticlesController < ApplicationController
 
     end
 
+  end
+
+  def update
+    @article = Article.find_by(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def show
